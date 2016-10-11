fea5246b39a2eaec2889fd923e36b0543dae4496
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-10-11 06:49:40 +0300

    ADd Narowwing, Widening Casting - class

--- src/main/java/com/yuliiakulyk/app/classwork/lesson3/NarrowingCastingRunner.java
+++ src/main/java/com/yuliiakulyk/app/classwork/lesson3/NarrowingCastingRunner.java
@@ -4,4 +4,8 @@ package com.yuliiakulyk.app.classwork.lesson3;
  * Created by Юлия on 08.10.2016.
  */
 public class NarrowingCastingRunner {
+    public static void main(String[] args) {
+        NarrowingCasting casting = new NarrowingCasting ();
+        casting.narrowing();
+    }
 }
--- src/main/java/com/yuliiakulyk/app/classwork/lesson3/WideningCasting.java
+++ src/main/java/com/yuliiakulyk/app/classwork/lesson3/WideningCasting.java
@@ -4,4 +4,16 @@ package com.yuliiakulyk.app.classwork.lesson3;
  * Created by Юлия on 08.10.2016.
  */
 public class WideningCasting {
-}
+    static byte byte1 = 127;
+    static short short1 = 0;
+    static int int1 = 0;
+    static long long1 = 0L;
+    static float float1 = 0.0f;
+    static double double1 = 0.0d;
+
+    public static void main (String[] args) {
+        double1 = float1 = long1 = int1 = short1 = byte1;
+        System.out.println(double1);
+    }
+
+    }
