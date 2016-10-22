59c69997b4fea7a32af750392304022f0a6a9c39
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-10-22 14:12:46 +0300

    Add arrays lesson7

---
+++ src/main/java/com/yuliiakulyk/runners/classwork/lesson7/Numbers0to10Printer.java
@@ -0,0 +1,15 @@
+package com.yuliiakulyk.runners.classwork.lesson7;
+
+import static java.lang.System.out;
+
+/**
+ * Created by 123 on 22.10.2016.
+ */
+public class Numbers0to10Printer {
+    public static void main (String args []) {
+        for (int i = 0; i <= 10; i++) {
+            out.print(i + " ");
+            //out.println(i);
+        }
+    }
+}
