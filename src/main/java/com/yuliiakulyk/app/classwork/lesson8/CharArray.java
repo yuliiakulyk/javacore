4d03017705e732c36742c5444787757bc4a9a924
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-10-29 14:01:04 +0300

    Add CharArray class to lesson8 classwork

---
+++ src/main/java/com/yuliiakulyk/app/classwork/lesson8/CharArray.java
@@ -0,0 +1,18 @@
+package com.yuliiakulyk.app.classwork.lesson8;
+
+import java.util.Arrays;
+
+import static java.lang.System.out;
+
+/**
+ * Created by Yuliia Kulyk on 29.10.2016.
+ */
+public class CharArray {
+    public static void main(String[] args) {
+        char [] array = new char[256];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (char) i;
+        }
+        out.println("Char array:\n" + Arrays.toString(array));
+    }
+}
