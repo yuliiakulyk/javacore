0d2dc13c752884c1a49af3e11e95615ee2555cf1
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-11-14 23:26:05 +0200

    Add problems 2 and 3 to Hometask13 runner

--- src/main/java/com/yuliiakulyk/runners/homework/lesson13/Hometask13.java
+++ src/main/java/com/yuliiakulyk/runners/homework/lesson13/Hometask13.java
@@ -1,5 +1,6 @@
 package com.yuliiakulyk.runners.homework.lesson13;
 
+import com.yuliiakulyk.app.classwork.lesson10.Palindrome;
 import com.yuliiakulyk.app.utils.convertors.FileWork;
 import com.yuliiakulyk.app.homework.lesson9.ArraySortAscDesc;
 import com.yuliiakulyk.app.homework.lesson12.ArrayParser;
@@ -16,13 +17,15 @@ import static java.lang.System.out;
 public class Hometask13 {
     public static void main(String[] args) {
         boolean doAgain = true;
+        FileWork fileWork = new FileWork();
         do {
             out.println("Please select number of a problem to solve:");
             out.println("1 - Read int arrays from a file, sort and write to another file.");
+            out.println("2 - Read lines in one file and save them in another file with indication if they are palindromes.");
+            out.println("3 - Read input from console and save it to file.");
             out.println("0 - Exit.");
             switch (getKeyboardInput()) {
                 case "1":
-                    FileWork fileWork = new FileWork();
                     ArraySortAscDesc sort = new ArraySortAscDesc();
                     ArrayParser parser = new ArrayParser();
                     String[] array = fileWork.readFileLines("src/test/resources/ArraysForFileWork.txt");
@@ -35,6 +38,27 @@ public class Hometask13 {
                         sortedArray[i] = sortedArray[i].replace(" ", "");
                     } // create array of String values that are sorted ints
                     fileWork.writeFileLines(sortedArray, "src/test/resources/ArraysForFileWorkSorted.txt");
+                    break;
+                case "2":
+                    Palindrome palindrome = new Palindrome();
+                    array = fileWork.readFileLines("src/test/resources/input.txt");
+                    String[] arrayPalindromes = new String[array.length];
+                    boolean isPalindrome;
+                    for (int i = 0; i < array.length; i++) {
+                        isPalindrome = palindrome.isPalindrome1(array[i]);
+                        if (isPalindrome) {
+                            arrayPalindromes[i] = array[i] + " is a palindrome.";
+                        } else {
+                            arrayPalindromes[i] = array[i] + " is not a palindrome.";
+                        }
+                    }
+                    fileWork.writeFileLines(arrayPalindromes, "src/test/resources/outputPalindromes.txt");
+                    out.println("Please see result in file src/test/resources/outputPalindromes.txt");
+                    break;
+                case "3":
+                    array = consoleToStringArray("exit");
+                    fileWork.writeFileLines(array, "src/test/resources/consoleToFile.txt");
+                    out.println("Please see result in file src/test/resources/consoleToFile.txt");
                     break;
                 case "0":
                     doAgain = false;
--- src/main/java/com/yuliiakulyk/runners/utils/KeyboardInput.java
+++ src/main/java/com/yuliiakulyk/runners/utils/KeyboardInput.java
@@ -1,6 +1,9 @@
 package com.yuliiakulyk.runners.utils;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.lang.System.out;
 
 /**
  * Created by Yuliia Kulyk on 12.11.2016.
@@ -10,6 +13,21 @@ public class KeyboardInput {
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
         return string;
+    }
+
+    public static String[] consoleToStringArray (String exitWord) {
+        String input;
+        ArrayList<String> arrayList = new ArrayList();
+        boolean doAgain = true;
+        do {
+            out.println("Enter a line. Type \"" + exitWord + "\" to stop adding lines and save to file.");
+            input = getKeyboardInput();
+            if (input.equals(exitWord)) {
+                break;
+            }
+            arrayList.add(input);
+        } while (true);
+        return arrayList.toArray(new String[arrayList.size()]);
     }
 
 }
