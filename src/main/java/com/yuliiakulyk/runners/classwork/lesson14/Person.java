6663d8bb1b3158ac5490acc44b3e8251098c9085
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-11-17 08:57:01 +0200

    Add lesson14 classwork

--- src/main/java/com/yuliiakulyk/runners/classwork/lesson14/Person.java
+++ src/main/java/com/yuliiakulyk/runners/classwork/lesson14/Person.java
@@ -4,4 +4,14 @@ package com.yuliiakulyk.runners.classwork.lesson14;
  * Created by Yuliia Kulyk on 16.11.2016.
  */
 public class Person {
+    protected String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
