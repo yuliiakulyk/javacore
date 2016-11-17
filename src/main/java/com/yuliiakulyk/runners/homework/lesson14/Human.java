b1e5be3e30f63eaf7c79c7b03e75b564f11c437e
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-11-17 10:57:59 +0200

    Add lesson14 homework

---
+++ src/main/java/com/yuliiakulyk/runners/homework/lesson14/Human.java
@@ -0,0 +1,33 @@
+package com.yuliiakulyk.runners.homework.lesson14;
+
+import static java.lang.System.out;
+
+/**
+ * Created by Yuliia Kulyk on 17.11.2016.
+ */
+public class Human {
+    private int age;
+    private String name;
+
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 1 && age <= 100) {
+            this.age = age;
+            out.println("New age is set.");
+        } else
+            out.println("Age invalid.");
+    }
+
+}
