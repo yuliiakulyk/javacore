7177e913e01c4fec58ae4c65570d6aa20d007928
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-11-21 23:50:08 +0200

    Add part of lesson15 homework

---
+++ src/main/java/com/yuliiakulyk/app/homework/lesson15/BicycleWithRingInterface.java
@@ -0,0 +1,8 @@
+package com.yuliiakulyk.app.homework.lesson15;
+
+/**
+ * Created by Yuliia Kulyk on 21.11.2016.
+ */
+public interface BicycleWithRingInterface {
+    public void ring();
+}
---
+++ src/main/java/com/yuliiakulyk/app/homework/lesson15/RandomArrayCreator.java
@@ -0,0 +1,19 @@
+package com.yuliiakulyk.app.homework.lesson15;
+
+import com.yuliiakulyk.app.homework.lesson7.RandomArrayGenerators;
+
+/**
+ * Created by Yuliia Kulyk on 21.11.2016.
+ */
+public class RandomArrayCreator {
+
+    public int[] create (int size) {
+        RandomArrayGenerators generators = new RandomArrayGenerators();
+        return generators.random1DimensionArray(size, 0, 100);
+    }
+
+    public int[][] create (int size1, int size2) {
+        RandomArrayGenerators generators = new RandomArrayGenerators();
+        return generators.random2DimensionArray(size1, size2, 0, 100);
+    }
+}
