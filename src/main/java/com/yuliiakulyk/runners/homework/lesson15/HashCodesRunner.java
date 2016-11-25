8dbb9bc6a3535fb0c93f32367fb1f2a0a17c1957
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-11-25 17:58:43 +0200

    Add unit tests for Enigma, hash code runner

---
+++ src/main/java/com/yuliiakulyk/runners/homework/lesson15/HashCodesRunner.java
@@ -0,0 +1,27 @@
+package com.yuliiakulyk.runners.homework.lesson15;
+
+import static java.lang.System.out;
+
+/**
+ * Created by Yuliia Kulyk on 25.11.2016.
+ */
+public class HashCodesRunner {
+    public class BlackBox {
+        int varA;
+        int varB;
+        public BlackBox(int varA, int varB) {
+            this.varA = varA;
+            this.varB = varB;
+        }
+    }
+
+    public static void main(String[] args) {
+        HashCodesRunner runner = new HashCodesRunner();
+        BlackBox box1 = runner.new BlackBox(5, 10);
+        BlackBox box2 = runner.new BlackBox(5, 10);
+        out.println("HashCode of box1 = " + box1.hashCode());
+        out.println("HashCode of box2 = " + box2.hashCode());
+        out.println("box1 equals box1 = " + box1.equals(box1));
+        out.println("box1 equals box2 = " + box1.equals(box1));
+    }
+}
