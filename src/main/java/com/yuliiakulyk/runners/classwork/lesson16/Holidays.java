ea930493387405bb5f18dd955b790e085ac64aac
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-11-26 14:05:05 +0200

    Add classwork16 Enumerators

---
+++ src/main/java/com/yuliiakulyk/runners/classwork/lesson16/Holidays.java
@@ -0,0 +1,31 @@
+package com.yuliiakulyk.runners.classwork.lesson16;
+
+import java.util.Arrays;
+
+/**
+ * Created by Yuliia Kulyk on 26.11.2016.
+ */
+public enum Holidays {
+    NEW_YEAR(1, "January", "New Year"), YULIIA_BIRTHDAY(28, "July", "Yuliia Birthday");
+    int date;
+    String month;
+    String name;
+
+    private Holidays(int date, String month, String name) {
+        this.date = date;
+        this.month = month;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        for (Holidays hName: Holidays.values()) {
+            System.out.println(hName.name + " holiday is celebrated on the "+ hName.date + "th of " + hName.month + ".");
+        }
+
+        Arrays.asList(Holidays.values()).forEach(holiday ->
+                System.out.println(holiday.name + " is celebrated on the " + holiday.date + "th of " + holiday.month + ".")
+        );
+
+    }
+
+}
---
+++ src/main/java/com/yuliiakulyk/runners/homework/lesson13/EnumHometask13.java
@@ -0,0 +1,8 @@
+package com.yuliiakulyk.runners.homework.lesson13;
+
+/**
+ * Created by Yuliia Kulyk on 26.11.2016.
+ */
+public enum EnumHometask13 {
+    A, B, C, D;
+}
