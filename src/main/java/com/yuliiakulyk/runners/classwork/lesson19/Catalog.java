41041334883ae3e2a26f602b16c0c3be138da018
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-12-03 14:00:04 +0200

    Add lesson19 classwork on collections

---
+++ src/main/java/com/yuliiakulyk/runners/classwork/lesson19/Catalog.java
@@ -0,0 +1,15 @@
+package com.yuliiakulyk.runners.classwork.lesson19;
+
+import java.util.Scanner;
+
+/**
+ * Created by Yuliia Kulyk on 03.12.2016.
+ */
+public class Catalog {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter category");
+        String category;
+
+    }
+}
