9f82921e33bfdec028c0bee0738033d6eba6c6f0
yuliiakulyk <yuliia.kulyk@gmail.com>
2016-12-06 11:46:20 +0200

    Add part of lesson19 homework

--- src/main/java/com/yuliiakulyk/runners/homework/lesson19/ContactRunner.java
+++ src/main/java/com/yuliiakulyk/runners/homework/lesson19/ContactRunner.java
@@ -15,25 +15,26 @@ public class ContactRunner {
 
     public static void main(String[] args) {
         ContactRunner runner = new ContactRunner();
-        FileWork fileWork = new FileWork();
-        String[] linesFromFile = fileWork.readFileLines("src/main/java/com/yuliiakulyk/runners/homework/lesson19/contacts.csv");
-        ArrayParser parser = new ArrayParser();
-        String[] contactData;
-        runner.map = new TreeMap();
-        ArrayList<Contact> contacts = new ArrayList<>();
-        for(String line: linesFromFile) {
-            contactData = parser.stringToStringArray(line, ",");
-            contacts = runner.map.get(contactData[0]);
-            contacts.add(new Contact(contactData[1], contactData[2]));
-            runner.map.put(contactData[0], contacts);
-        }
+//        FileWork fileWork = new FileWork();
+//        String[] linesFromFile = fileWork.readFileLines("src/main/java/com/yuliiakulyk/runners/homework/lesson19/contacts.csv");
+//        ArrayParser parser = new ArrayParser();
+//        String[] contactData;
+//        runner.map = new TreeMap();
+//        ArrayList<Contact> contacts = new ArrayList<>();
+//        for(String line: linesFromFile) {
+//            contactData = parser.stringToStringArray(line, ",");
+//            contacts = runner.map.get(contactData[0]);
+//            contacts.add(new Contact(contactData[1], contactData[2]));
+//            runner.map.put(contactData[0], contacts);
+//        }
+        runner.readContactsFromFile();
         runner.printCategories(runner.map);
         String categoryInput;
         do {
             System.out.println("Please enter a category to work with:");
             categoryInput = getKeyboardInput();
             if (categoryInput.equalsIgnoreCase("exit")) {
-                // write everything to the file here.
+                runner.writeContactsToFile();
                 System.out.println("Work is finished.");
                 break;
             }
@@ -47,9 +48,9 @@ public class ContactRunner {
     }
 
     public void printCategories(TreeMap map) {
-        Set set = map.keySet();
-        for(Object item: set) {
-            System.out.println(item.toString());
+        Set<String> set = map.keySet();
+        for(String item: set) {
+            System.out.println(item);
         }
     }
 
@@ -79,22 +80,46 @@ public class ContactRunner {
 
     public void readContactsFromFile() {
         FileWork fileWork = new FileWork();
-        String[] linesFromFile = fileWork.readFileLines("src/main/java/com/yuliiakulyk/runners/homework/lesson19/contacts.csv");
-        ArrayList<String[]> linesParsed = new ArrayList<>();
+        // строки из файла:
+        String[] wholeLinesFromFile = fileWork.readFileLines("src/main/java/com/yuliiakulyk/runners/homework/lesson19/contacts.csv");
+        ArrayList<String[]> parsedLinesFromFile = new ArrayList<>();
         ArrayParser parser = new ArrayParser();
-        for (int i = 0; i < linesFromFile.length; i++) {
-            linesParsed.add(i, parser.stringToStringArray(linesFromFile[i], ","));
+        for (int i = 0; i < wholeLinesFromFile.length; i++) { //добавили массивы разделенных строк в
+            parsedLinesFromFile.add(i, parser.stringToStringArray(wholeLinesFromFile[i], ","));
         }
-        for (int j = 0; j < linesParsed.size(); j++) {
-            
-        }
-        String[] contactData;
         this.map = new TreeMap();
-        for(String line: linesFromFile) {
-            contactData = parser.stringToStringArray(line, ",");
-            Set set = map.keySet();
+        ArrayList<Contact> currentCategoryContacts;
+        String currentCategory;
+        Iterator iterator;
+        String[] currentLineStrings;
+        while (parsedLinesFromFile.size() != 0) {
+            currentCategoryContacts = new ArrayList<>();
+            currentCategory = parsedLinesFromFile.get(0)[0];
+            iterator = parsedLinesFromFile.iterator();
+            while (iterator.hasNext()) {
+                currentLineStrings = (String[]) iterator.next();
+                if (currentCategory.equalsIgnoreCase((currentLineStrings[0]))) {
+                    currentCategoryContacts.add(new Contact(currentLineStrings[1], currentLineStrings[2]));
+                    iterator.remove();
+                }
+                this.map.put(currentCategory, currentCategoryContacts);
+            }
+        }
+
+    }
+
+    public void writeContactsToFile() {
+        Set<String> set = this.map.keySet();
+        FileWork fileWork = new FileWork();
+        ArrayList<String> linesToWrite = new ArrayList<>();
+        ArrayList<Contact> contactsInCategory;
+        for(String category: set) {
+            contactsInCategory = this.map.get(category);
+            for (Contact contact: contactsInCategory) {
+                linesToWrite.add(category + "," + contact.getName() + "," + contact.getPhone());
+            }
 
         }
-        //this.map.put(contactData[0], new Contact(contactData[1], contactData[2]));
+        fileWork.writeFileLines((String[]) linesToWrite.toArray(), "src/main/java/com/yuliiakulyk/runners/homework/lesson19/contacts.csv");
     }
 }
