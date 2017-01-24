a5debc50973429d5a8d478ad857babf44af683ca
yuliiakulyk <yuliia.kulyk@gmail.com>
2017-01-24 11:44:45 +0200

    Add test with Explicit wait until element is present - anywayanyday.com

---
+++ src/test/java/com/yuliiakulyk/app/classwork/lesson27/pages/AbstractPage.java
@@ -0,0 +1,37 @@
+package com.yuliiakulyk.app.classwork.lesson27.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by Yuliia Kulyk on 21.01.2017.
+ */
+public class AbstractPage {
+        WebDriver driver;
+        public static final String BASE_URL = "https://dev.equerest.com/";
+
+        public AbstractPage(WebDriver driver, String url){
+            this.driver = driver;
+            driver.navigate().to(url);
+        }
+        public AbstractPage(WebDriver driver){
+            this.driver = driver;
+        }
+
+        public void clickJS(By locator){
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", driver.findElement(locator));
+        }
+
+        public void scrollTo(WebElement element) {
+
+            String scrollElementIntoMiddle =
+                    "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);"
+                            + "var elementTop = arguments[0].getBoundingClientRect().top;"
+                            + "window.scrollBy(0, elementTop-(viewPortHeight/2));";
+
+            ((JavascriptExecutor) driver).executeScript(scrollElementIntoMiddle, element);
+        }
+
+}
---
+++ src/test/java/com/yuliiakulyk/app/homework/lesson27/ExplicitWaits.java
@@ -0,0 +1,43 @@
+package com.yuliiakulyk.app.homework.lesson27;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Yuliia Kulyk on 23.01.2017.
+ */
+public class ExplicitWaits {
+
+    @Test
+    public void WaitUntilElementVisible() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.anywayanyday.com/");
+        driver.findElement(By.cssSelector("div.ui-input.departure-airport.col50>div>input"))
+                .sendKeys("KBP");
+        driver.findElement(By.cssSelector("div.ui-input.arrival-airport.col50>div>input"))
+                .sendKeys("AMS");
+        driver.findElement(By.cssSelector("div.ui-input.ui-input-date.date.col100")).click();
+        WebDriverWait waitDate = new WebDriverWait(driver, 2);
+        WebElement calendarDate = waitDate.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("td.day.today")));
+        calendarDate.click();
+        driver.findElement(By.xpath("(//a[.='Найти'])[1]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("div.Growl-Logo")));
+        WebElement result = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a.ticket-link")));
+        result.click();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        Assert.assertTrue(driver.findElement(By.cssSelector("div.payment-action_button")).isEnabled());
+        driver.quit();
+    }
+}
