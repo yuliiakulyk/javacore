035361bfef8a121c61c90ca900cf6d28173155f9
yuliiakulyk <yuliia.kulyk@gmail.com>
2017-03-09 18:26:11 +0200

    Add tests on checking if input has focus, geo suggestions of airport codes

--- src/test/java/com/yuliiakulyk/app/anywayanyday/MainPage.java
+++ src/test/java/com/yuliiakulyk/app/anywayanyday/MainPage.java
@@ -1,14 +1,25 @@
 package com.yuliiakulyk.app.anywayanyday;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Yuliia Kulyk on 08.03.2017.
  */
 public class MainPage extends BasePage {
     public By inputAirportFrom = By.cssSelector("input.ui-input-filed.city-field");
+    public By inputAirportTo = By.cssSelector("div.ui-input.arrival-airport.col100>div>input");
+    public By geoSuggest = By.cssSelector("ul.geo-suggest>li");
+    public By recognizedAirportCodeCityFrom = By.cssSelector("div.ui-input.departure-airport.col100>span.airport-code");
+    public By recognizedAirportCodeCityTo = By.cssSelector("div.ui-input.arrival-airport.col100>span.airport-code");
+
 
     public MainPage(WebDriver driver) {
         super(driver);
@@ -19,5 +30,33 @@ public class MainPage extends BasePage {
         element.click();
         WebElement activeElement = driver.switchTo().activeElement();
         return element.getAttribute("placeholder").equals(activeElement.getAttribute("placeholder"));
+    }
+
+    public int numberOfGeoSuggests(String city, By inputField) {
+        driver.findElement(inputField).sendKeys(city);
+        List<WebElement> suggestions = driver.findElements(geoSuggest);
+        return suggestions.size();
+    }
+
+    @Step("Check airpost with code [1]")
+    public void checkOneCode(By inputField, String code, By recognizedCode, ArrayList<String> unrecognizedCodes) {
+        WebElement field = driver.findElement(inputField);
+        field.clear();
+        field.sendKeys(code);
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        field.sendKeys(Keys.ENTER);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        WebElement element = driver.findElement(recognizedCode);
+        if (!element.getText().equals(code)) {
+            unrecognizedCodes.add(code);
+        }
     }
 }
--- src/test/java/com/yuliiakulyk/app/anywayanyday/MainPageTest.java
+++ src/test/java/com/yuliiakulyk/app/anywayanyday/MainPageTest.java
@@ -1,10 +1,16 @@
 package com.yuliiakulyk.app.anywayanyday;
 
+import com.yuliiakulyk.runners.convertors.and.files.classes.FileWork;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Yuliia Kulyk on 08.03.2017.
@@ -22,5 +28,35 @@ public class MainPageTest extends BaseTest {
     public void checkCityFromActive() {
         Assert.assertTrue(page.checkFieldActive(page.inputAirportFrom));
     }
+
+    @Test
+    public void checkCityToActive() {
+        Assert.assertTrue(page.checkFieldActive(page.inputAirportTo));
+    }
+
+    @Test
+    public void checkCityFromMultipleSuggestions() {
+        Assert.assertTrue(page.numberOfGeoSuggests("Paris", page.inputAirportFrom) >= 2);
+    }
+
+    @Test
+    public void checkCityToMultipleSuggestions() {
+        Assert.assertTrue(page.numberOfGeoSuggests("Paris", page.inputAirportTo) >= 2);
+    }
+
+    @Test
+    public void checkRecognitionOfAirportCodesCityFrom() {
+        FileWork fileWork = new FileWork();
+        String[] airportCodes = fileWork.readFileLines("src/test/java/com/yuliiakulyk/app/anywayanyday/airportcodes.txt");
+        ArrayList<String> unrecognizedCodes = new ArrayList<>();
+        for (int i = 0; i < airportCodes.length; i++) {
+            page.checkOneCode(page.inputAirportFrom, airportCodes[i], page.recognizedAirportCodeCityFrom, unrecognizedCodes);
+        }
+        //System.out.println(unrecognizedCodes.toArray().toString());
+        System.out.println(Arrays.toString(unrecognizedCodes.toArray()));
+        Assert.assertEquals(0, unrecognizedCodes.size());
+    }
+
+
 
 }
